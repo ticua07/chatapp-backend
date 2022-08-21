93f626c744d80f8e5e6edfb6ec9dd39e183d6e17
Ticua <ticua@ticua.ar>
2022-08-21 17:29:12 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import asyncio
+import json
+from uuid import uuid4
+from websockets import serve
+
+from utils import ChatGroup
+
+groups = []
+
+
+async def echo(websocket, path):
+    async for message in websocket:
+        # TODO: Create chats, get 404 when chatID is not a ChatGroup
+        # TODO: Get chats, maybe people too?
+        # TODO: Delete chat when 0 people are available
+        if not (len(groups) > 0):
+            groups.append(
+                {
+                    # "id": uuid4().replace("-", ""),
+                    "id": "1",
+                    "group": ChatGroup("Chat 1", "Chat 1 only!!!"),
+                }
+            )
+        converted_message = json.loads(message)
+        converted_path = path[1:]
+        match converted_message["type"]:
+            case "connect":
+                print("client has connected")
+                for group in groups:
+                    if group["id"] == converted_path:
+                        await group["group"].Connect(websocket)
+
+            case "newMessage":
+                print("Got new message: " + converted_message["content"])
+                for group in groups:
+                    if group["id"] == converted_path:
+                        await group["group"].SendMessage(
+                            author=converted_message["author"],
+                            message=converted_message["content"],
+                        )
+
+            case _:
+                print("didn't understand nathing brooo")
+
+    # Client closed website
+    for group in groups:
+        if group["id"] == converted_path:
+            group["group"].Disconnect(websocket)
+
+
+async def main():
+    async with serve(echo, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+
+asyncio.run(main())
---
+++ utils.py
@@ -0,0 +1,41 @@
+import json
+from uuid import uuid4
+
+
+class ChatGroup:
+    def __init__(self, title: str, description: str):
+        # self.id = uuid4()
+        self.title = title
+        self.description = description
+        self.connected = []
+        self.messages = []
+
+    def GetMessages(self, lastN):
+        last_messages = self.messages[len(self.messages) - lastN : len(self.messages)]
+        return last_messages
+
+    async def SendMessage(self, author, message):
+        print(f"sending to {len(self.connected)} people")
+        new_message = {"author": author, "content": message}
+        self.messages.append(new_message)
+        for conn in self.connected:
+            await conn["socket"].send(json.dumps(new_message | {"type": "newMessage"}))
+
+    async def Connect(self, ws):
+        randomIdentifier = str(uuid4()).split("-")[0]
+        self.connected.append({"socket": ws, "id": randomIdentifier})
+        # self.connected.append(ws)
+        await ws.send(json.dumps({"type": "identify", "randomID": randomIdentifier}))
+
+    def Disconnect(self, ws):
+        try:
+            # self.connected.remove(ws)
+            print(f"previous member (removing) {len(self.connected)}")
+            for conn in self.connected:
+                if conn["socket"] == ws:
+                    self.connected.remove(conn)
+                    print("ChatGroup member has disconnected")
+
+            print(f"after members (removing) {len(self.connected)}")
+        except ValueError:
+            print("couldn't remove websocket")
